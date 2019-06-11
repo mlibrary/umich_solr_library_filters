aa41dc239c9629dc99ec9402d52f3a8e85f83e10
Bill Dueber <bill@dueber.com>
2019-06-11 12:35:30 -0400

    Fix tests to reflect 6-digit initial number

--- src/test/java/edu/umich/lib/converters/LCCallNumberNormalizerTest.java
+++ src/test/java/edu/umich/lib/converters/LCCallNumberNormalizerTest.java
@@ -15,12 +15,12 @@ public class LCCallNumberNormalizerTest {
     @Test
     public void error_case_1()  {
         String s = "HD5325.R12.1894. C22";
-        assertEquals(s,"HD@@05325@R@12000@18940@C@22000", test_normalization(s));
+        assertEquals(s,"HD@@005325@R@12000@18940@C@22000", test_normalization(s));
     }
 
     @Test
     public void error_case_2() {
         String s = "BR75 .H4453.1674";
-        assertEquals(s, "BR@@00075@H@44530@16740", test_normalization(s) );
+        assertEquals(s, "BR@@000075@H@44530@16740", test_normalization(s) );
     }
 }
